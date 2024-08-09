5e5df25d9c01dbabfe922e2783d22f0931f03ae6
rohitkoli04 <167304424+rohitkoli04@users.noreply.github.com>
2024-08-09 18:02:31 +0530

    Delete ASSIGNMENT/M_3 OOPs Concepts directory

--- ASSIGNMENT/M_3 OOPs Concepts/New folder/que2.cpp
+++
@@ -1,4 +0,0 @@
-/* Write a program of Addition, Subtraction, Division, Multiplication using
-constructor*/
-#include<iostream>
-using namespace std;
--- ASSIGNMENT/M_3 OOPs Concepts/opps1/que1.cpp
+++
@@ -1,7 +0,0 @@
-//WAP to print “Hello World” using C++
-#include<iostream>
-using namespace std;
-int main(){
-    cout<<"hello world"<<endl;
-    
-}
